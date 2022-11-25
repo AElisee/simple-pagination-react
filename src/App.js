35cb8b9ceb5b862729a3aa6f3b6ce65944abdc70
AElisee <2kange.eli@gamil.com>
2022-11-25 17:02:05 +0000

    pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Meals from "./components/Meals";
+import Pagination from "./components/Pagination";
 
-function App() {
+const App = () => {
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // User is currently on this page
+  const [currentPage, setCurrentPage] = useState(1);
+  // No of Records to be displayed on each page
+  const [recordsPerPage] = useState(10);
+
+  const getData = () => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+      .then((res) => {
+        setMeals(res.data.meals);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(meals);
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  // Records to be displayed on the current page
+  const currentRecords = meals.slice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(meals.length / recordsPerPage);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {loading ? (
+        <div
+          style={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontWeigth: 900,
+            fontSize: "24px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <p>chargement... !</p>
+        </div>
+      ) : (
+        <div>
+          <Meals meals={currentRecords} currentPage={currentPage} />
+          <Pagination
+            nPages={nPages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Meals.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Meals = ({ meals, currentPage }) => {
+  return (
+    <div>
+      <div className="header">
+        <h1>
+          <span>s</span>imple Pagination react
+        </h1>
+        <p>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            lien tuto
+          </a>
+        </p>
+      </div>
+      <h2>Meals</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>N°</th>
+            <th>Name</th>
+            <th>Origin</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {meals.map((meal, index) => (
+            <tr key={index}>
+              <td>
+                {currentPage == 1
+                  ? index + 1
+                  : index + 1 > 9
+                  ? (currentPage + 1).toString() + 0
+                  : currentPage.toString() + (index + 1)}
+              </td>
+              <td> {meal.strMeal}</td>
+              <td> {meal.strArea}</td>
+              <td> {meal.strCategory}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Meals;
